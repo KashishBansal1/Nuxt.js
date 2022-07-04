097c592815958def8b55b61bdc96168c5f5cf13d
Kashish Bansal <aashma.2008@gmail.com>
2022-07-05 00:19:08 +0530

    created a nuxt app

---
+++ my-app/components/home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>
+            Welcome
+        </h1>
+        <p>
+            This is my homepage!
+        </p>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'HomePage'
+}
+</script>
